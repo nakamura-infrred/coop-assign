3b63f8c8f5084b970547d3ecf7da1d6d4e512bef
toshi <nakamura@infr.red>
2025-10-21 12:54:32 +0900

    feat: integrate fullcalendar and task provider

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -3,13 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { AuthProvider } from './providers/AuthProvider.tsx'
 import { StorageProvider } from './providers/StorageProvider.tsx'
+import { TaskProvider } from './providers/TaskProvider.tsx'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <AuthProvider>
       <StorageProvider>
-        <App />
+        <TaskProvider>
+          <App />
+        </TaskProvider>
       </StorageProvider>
     </AuthProvider>
   </StrictMode>,
